f9e40b6cee2f3a48a9ec7b8a2dd9e06e2cbc8497
Evgeny Sysoletin <evgeny.sysoletin@gmail.com>
2019-01-17 18:41:59 +0300

    feat: update saving in ls

--- src/index.js
+++ src/index.js
@@ -2,17 +2,25 @@ import {render} from 'react-dom';
 import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
 import thunk from 'redux-thunk';
-import reducer, {initialState} from './store/reducer';
+import reducer from './store/reducer';
 import App from './components/App';
+import {initialState as sourcesIS} from './components/Sources/reducer';
 
 const stateFromLS = localStorage.getItem('storeState');
 const preloadStore =
-  stateFromLS !== null ? JSON.parse(stateFromLS) : initialState;
+  stateFromLS !== null
+    ? {sources: {...sourcesIS, ...JSON.parse(stateFromLS)}}
+    : undefined;
 
 const store = createStore(reducer, preloadStore, applyMiddleware(thunk));
 
 store.subscribe(() => {
-  localStorage.setItem('storeState', JSON.stringify(store.getState()));
+  localStorage.setItem(
+    'storeState',
+    JSON.stringify({
+      sortingValue: store.getState().sources.sortingValue
+    })
+  );
 });
 
 render(
