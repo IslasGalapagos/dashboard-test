1e3f9bcfea5800be35230ba3ae2e5f2854926a40
Evgeny Sysoletin <evgeny.sysoletin@gmail.com>
2019-01-18 16:12:20 +0300

    feat(server): add points and do refactoring

--- server/index.js
+++ server/index.js
@@ -3,24 +3,31 @@ require('dotenv').config();
 const express = require('express');
 const request = require('superagent');
 
-const app = express();
+const requestF = async (req, res) => {
+  const point = req.path.replace('/api', '');
 
-app.get('/api/sources/', async (req, res) => {
   const {body: result} = await request
-    .get(`${process.env.API_DOMAIN}/sources/`)
+    .get(`${process.env.API_DOMAIN}${point}`)
     .set('Cookie', `access_token=${process.env.API_TOKEN}`)
     .query({
-      project_id: '5600',
-      offset: '0',
-      limit: '9999999',
-      date_from: '2018-02-13',
-      date_to: '2018-02-19',
-      format: 'json'
+      ...{
+        project_id: '5600',
+        offset: '0',
+        limit: '9999999',
+        format: 'json'
+      },
+      ...req.query
     });
 
   res.set('Access-Control-Allow-Origin', '*');
   res.send(result);
-});
+};
+
+const app = express();
+
+app.get('/api/sources/', requestF);
+app.get('/api/chart/sources/', requestF);
+app.get('/api/chart/campaigns/', requestF);
 
 app.listen(3000, function() {
   console.log('Example app listening on port 3000!');
