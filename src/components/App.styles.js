82445965ed0b75520c5f107a8b40c476b6cfdebd
Evgeny Sysoletin <evgeny.sysoletin@gmail.com>
2019-01-18 16:13:25 +0300

    feat: update common styles

--- src/components/App.styles.js
+++ src/components/App.styles.js
@@ -3,5 +3,31 @@ export const commonStyles = {
     fontFamily: '"Helvetica Neu",Helvetica,Arial',
     padding: '25px',
     backgroundColor: '#f9f9f9'
+  },
+
+  '.col': {
+    display: 'inline-block',
+    verticalAlign: 'top',
+    width: '50%',
+    boxSizing: 'border-box'
+  },
+
+  '.col.-left': {
+    paddingRight: '50px'
+  },
+
+  '.col.-right': {
+    paddingLeft: '50px'
+  },
+
+  '.section_title': {
+    fontSize: '24px',
+    fontWeight: 'normal',
+    margin: '0 0 12px 0',
+    color: '#666'
+  },
+
+  '.loading': {
+    color: '#8080ff'
   }
 };
